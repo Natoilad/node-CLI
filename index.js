f9b44d345e8526b520e56c6343fab9d322d98d2d
Natoilad <109770565+Natoilad@users.noreply.github.com>
2023-05-05 01:55:38 +0300

    func

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const fs = require('fs/promises');
 
+const addText = async () => {
+  await fs.appendFile('file.txt', '\nappendtext');
+};
+// addText();
+
+const innerText = async () => {
+  const result = await fs.writeFile('file.txt', 'file');
+  //   console.log(result);
+};
+innerText();
 const readfile = async () => {
   const data = await fs.readFile('file.txt', 'utf-8');
   console.log(data);
 };
 readfile();
+
 // fs.readFile('file.txt')
 //   .then(data => console.log(data))
 //   .catch(error => console.log(error.message));
@@ -13,5 +24,6 @@ console.log('Welcome to Hell in NODE.js ');
 
 const contacts = require('./contacts');
 const { error } = require('console');
+const { appendFile } = require('fs');
 
 // console.log(contacts);
